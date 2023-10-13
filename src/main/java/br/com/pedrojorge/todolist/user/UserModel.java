89d6f35aaa247fef8b52952b30502022818339bd
Pedro Jorge <45081159+pedrkw@users.noreply.github.com>
2023-10-13 13:17:19 -0300

    feat: Implemented database interaction

--- src/main/java/br/com/pedrojorge/todolist/user/UserModel.java
+++ src/main/java/br/com/pedrojorge/todolist/user/UserModel.java
@@ -1,7 +1,28 @@
 package br.com.pedrojorge.todolist.user;
 
+import java.util.UUID;
+import java.time.LocalDateTime;
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity(name = "clients")
 public class UserModel {
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    private UUID id;
+
+    @Column(unique = true)
     private String username;
     private String name;
     private String password;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
 }
